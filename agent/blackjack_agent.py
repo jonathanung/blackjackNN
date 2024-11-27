d8b9eaafbdbd2092b5c9587827611d2d3e62306e
Jonathan Ung <jonathan.keith.ung@example.com>
2024-11-26 22:02:42 -0800

    revert memory and batch size

--- agent/blackjack_agent.py
+++ agent/blackjack_agent.py
@@ -113,8 +113,8 @@ class DeepQLearningAgent:
         self.criterion = nn.MSELoss()
         
         # Experience Replay
-        self.memory = deque(maxlen=100000)
-        self.batch_size = 10000
+        self.memory = deque(maxlen=10000)
+        self.batch_size = 1000
         self.parallel_memory = []  # Temporary storage for parallel experiences
         
         # Exploration parameters
